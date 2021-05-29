87c9dbde2e688c1d39d7c6fa316d618f1db6982f
Winicius Souza <winicius.souza@nvoip.com.br>
2021-05-29 20:56:04 -0300

    Update
    ícone de pausa

--- components/button/button.component.jsx
+++ components/button/button.component.jsx
@@ -7,11 +7,16 @@ const styles = StyleSheet.create({
     container: {
       paddingTop: 5,
     },
-    icon: {
+    iconStart: {
       width: 40,
       height: 40,
       marginLeft: 120, 
     },
+    iconPause: {
+      width: 40,
+      height: 40,
+      marginLeft: 120, 
+    }
   });
 
 //ícone pause https://image.flaticon.com/icons/png/512/27/27340.png
@@ -20,9 +25,16 @@ const ButtonComponent = () => {
     return (
         <View style={styles.container}>
         <Image
-          style={styles.icon}
+          style={styles.iconStart}
           source={{
             uri: 'https://img-premium.flaticon.com/png/512/149/149657.png?token=exp=1622332029~hmac=74506ec4d1b579bbddffce0ef9ba3df9',
+          }}
+        />
+        <Image
+          style={styles.iconPause}
+          hidden
+          source={{
+            uri: 'https://image.flaticon.com/icons/png/512/27/27340.png',
           }}
         />
       </View>
